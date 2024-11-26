172d0653b045a9bdd944edb4faaabef98529e9b9
Travis Henning <henningta@gmail.com>
2024-11-25 21:28:19 -0500

    Configured vite to use swc

--- packages/site/vite.config.ts
+++ packages/site/vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [
+    react(),
     nodePolyfills({
       include: ['events'],
     }),
